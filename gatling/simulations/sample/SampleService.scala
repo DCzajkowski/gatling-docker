7f8c1fe2b8e37c91f75491c83b3121cbc44ed6fa
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-12-28 00:52:30 +0100

    WS

--- gatling/simulations/sample/SampleService.scala
+++ gatling/simulations/sample/SampleService.scala
@@ -8,23 +8,24 @@ import io.gatling.jdbc.Predef._
 class SampleService extends Simulation {
 
 	val httpProtocol = http
-		.baseURL("http://sample-service:8000")
+		.baseURL("http://34.245.80.225:80")
 		.acceptHeader("*/*")
 
 	val scn = scenario("Sample Service Root")
 		.exec(http("request_1")
-			.get("/"))
+		.get("/boards"))
 
 	setUp(
     scn.inject(
 			nothingFor(2 seconds)
-      // ,atOnceUsers(10)
-			// ,nothingFor(4 seconds)
-      ,rampUsers(10) over(10 seconds)
-			,nothingFor(5 seconds)
-      // ,rampUsers(100) over(10 seconds)
-			// ,nothingFor(5 seconds)
-     ,rampUsersPerSec(30) to 300 during(10 minutes)
+      // , atOnceUsers(10)
+			// , nothingFor(4 seconds)
+      , rampUsers(10) over(10 seconds)
+			, nothingFor(5 seconds)
+      // , rampUsers(100) over(10 seconds)
+			// , nothingFor(5 seconds)
+      , rampUsersPerSec(30) to 300 during(10 minutes)
     )
   ).protocols(httpProtocol)
+
 }
